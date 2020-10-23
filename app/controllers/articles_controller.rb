523467859277e20b5de4d1e06b1b98126c7c36ee
Sourn Sophois <sournsophois@gmail.com>
2020-10-23 23:13:01 +0700

    Ruby Form helper--Actions: New and Create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,23 @@ class ArticlesController < ApplicationController
     # byebug
     @article = Article.all
   end
+
+  def new
+    # create this instance variable when the form is first loaded.
+    @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = 'The record is saved successfully.'
+      redirect_to article_path(@article) # redirect_to @ article is shorthand of redirect_to article_path(@article)
+    else
+      # call the new action when the form data cannot be saved
+      render 'new'
+    end
+
+  end
 end
